f453d2af7c94a64e2effd30a91caf21ac6b4a727
study_electronic <84219715+kimgeonoh@users.noreply.github.com>
2023-04-14 15:13:18 +0900

    Create 주행거리 비교하기.c

---
+++ 1star/주행거리 비교하기.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int A, B;
+	scanf("%d %d", &A, &B);
+
+	if (A > B) {
+		printf("A");
+	}
+
+	else if (A < B) {
+		printf("B");
+	}
+
+	else {
+		printf("same");
+	}
+
+	return 0;
+}
